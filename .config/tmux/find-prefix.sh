45cbcf8f0a65246764f646a3c894b5687546397e
dots <dots@harrybrwn.com>
2022-12-06 19:05:37 -0800

    [update] find-prefix.sh

--- .config/tmux/find-prefix.sh
+++ .config/tmux/find-prefix.sh
@@ -6,9 +6,7 @@
 #fi
 
 if [ -n "$SSH_CLIENT" ] && [ -n "$SSH_TTY" ]; then
-  echo "[$(date)] logged in via ssh" >> ~/tmux.log
   tmux set -g prefix C-b
 else
-  echo "[$(date)] logged in via tty" >> ~/tmux.log
   tmux set -g prefix C-f
 fi

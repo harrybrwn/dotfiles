fa9c5cea4ddc4a44ef1366fe05037bb17f80a91f
dots <dots@gopkgs.hrry.dev>
2025-06-01 11:25:50 -0700

    [add] config.jsonc, startup, bluetooth.sh

---
+++ .config/polybar/scripts/bluetooth.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eu
+
+case "${1:-}" in
+	check)
+		systemctl status bluetooth.service >/dev/null
+		;;
+
+	toggle)
+		if bluetoothctl show | grep -q 'Powered: yes'; then
+			bluetoothctl power off
+		else
+			bluetoothctl power on
+		fi
+		;;
+
+	status)
+		if bluetoothctl show | grep -q 'Powered: yes'; then
+			if echo info | bluetoothctl | grep -q '^Device'; then
+				#echo "%{F#2193ff}󰂱"
+				echo "%{F#1079de}󰂱"
+			else
+				echo "󰂯"
+			fi
+		else
+			echo "%{F#66ffffff}󰂲"
+		fi
+		;;
+
+	*)
+		echo "unknown command"
+		exit 1
+		;;
+esac

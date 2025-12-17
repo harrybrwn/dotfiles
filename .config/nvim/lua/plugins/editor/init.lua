9b7cf86eae4194d6386c5cbd30db7b54e30667d0
dots <dots@gopkgs.hrry.dev>
2025-12-16 20:04:18 -0800

    [update] init.lua

--- .config/nvim/lua/plugins/editor/init.lua
+++ .config/nvim/lua/plugins/editor/init.lua
@@ -1,4 +1,17 @@
-local mods = {}
+local mods = {
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    -- enabled = true,
+    opts = {
+      signs = true,
+      sign_priority = 1,                  -- don't place icons on top of git signs
+      highlight = {
+        pattern = [[.*<(KEYWORDS)>\s*:]], -- default: [[.*<(KEYWORDS)\s*:]]
+      },
+    },
+  },
+}
 
 vim.list_extend(mods, require("plugins.editor.treesitter"))
 vim.list_extend(mods, require("plugins.editor.diagnostics"))
